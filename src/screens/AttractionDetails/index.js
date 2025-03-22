fa2c0b2688a40d118dd5a65fd654d203236e2113
Akshita2020 <akshita.tiwari2020@gmail.com>
2025-03-22 18:37:36 +0530

    added backgorund image

--- src/screens/AttractionDetails/index.js
+++ src/screens/AttractionDetails/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ImageBackground} from 'react-native';
 import styles from './styles';
 
 const AttreactionDetails = ({navigation, route}) => {
   const {item} = route?.params || {};
+  const mainImage = item?.images?.length ? item.images[0] : null;
   const onBack = () => {
     navigation.goBack();
   };
   return (
-    <View>
+    <View style={styles.conatiner}>
+      <ImageBackground
+        style={styles.mainImage}
+        imageStyle={{borderRadius: 20}}
+        source={{uri: mainImage}}
+      />
       <Text onPress={onBack}>Go Back</Text>
       <Text>{item.name}</Text>
     </View>
--- src/screens/AttractionDetails/styles.js
+++ src/screens/AttractionDetails/styles.js
@@ -1,25 +1,15 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
+
+const {height} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  // },
-  subtitle: {
-    fontSize: 20,
-    color: '#000000',
-    marginTop: 20,
-    marginBottom: 10,
+  conatiner: {
+    margin: 32,
   },
-  row: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+  mainImage: {
+    width: '100%',
+    height: height / 2,
   },
-  emptyText:{
-    fontSize:20,
-    textAlign:'center',
-    marginTop:40,
-    color: 'rgba(0,0,0,0.5)',
-  }
 });
 
 export default styles;
